0371b2aa416a3b3566d8230e357034c0b7a44e83
Poonsak Rojanapong <poonsak.ro@ku.th>
2025-07-29 14:18:16 +0700

    p

---
+++ assignments/week04/lab01.py
@@ -0,0 +1,26 @@
+"""
+Personal Information Manager 
+
+#Create a tuple to store a person's basic info: (name, age, city, country)
+#Create a list to store their hobbies
+
+Allow the user to:
+
+Display all information
+Add new hobbies
+Remove hobbies
+Update age (by creating a new tuple)
+
+"""
+
+# Complete this program
+def personal_info_manager():
+    # Create initial person tuple
+    person = ("", 0, "", "")  # name, age, city, country
+    hobbies = []
+    
+    # Your code here
+    pass
+
+if __name__ == "__main__":
+    personal_info_manager()
