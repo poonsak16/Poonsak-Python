04e7cd3db5d2b749226f2d7ab16ac25439a91ff2
Poonsak Rojanapong <poonsak.ro@ku.th>
2025-09-23 14:53:39 +0700

    p

---
+++ assignments/week09/lab01.py
@@ -0,0 +1,46 @@
+"""
+Question 1: Beginner Number Guessing Game
+
+Create a simple number guessing game with these requirements:
+
+Random number between 1-20
+    Maximum 6 attempts
+    Show remaining attempts after each guess
+    Display appropriate win/lose messages
+    Validate numeric input only
+    
+Example 
+
+    === SIMPLE GUESSING GAME ===
+    Guess my number between 1 and 20!
+    You have 6 attempts.
+
+    Attempt 1/6 - Enter your guess: 10
+    Too low! Try again.
+
+    Attempt 2/6 - Enter your guess: 15
+    Too high! Try again.
+
+    Attempt 3/6 - Enter your guess: 12
+    Congratulations! You won in 3 attempts!
+
+"""
+
+import random
+
+random_number = random.randint(1, 20)
+print("=== SIMPLE GUESSING GAME ===")
+print("Guess my number between 1 and 20!")
+print("You have 6 attempts.")
+
+for i in range(6):
+    guess_number = int(input("Attemp {i+1}/6 - Enter your guess : "))
+
+    if random_number == guess_number:
+        print(f"Congratulations! You won in {i+1} attemp")
+        break
+    elif random_number < guess_number:
+        print("Too high! Try again.")
+    else:
+        print("Too low! Try again. ")
+        
--- assignments/week09/test_random.py
+++ assignments/week09/test_random.py
@@ -1,7 +1,23 @@
 import random
 
 def test_random():
-    random_number = random.randint(1, 100)
-    print(random_number)
+    # สร้างตัวแปร random_number ตัวแปรคือการสุ่มเลขระหว่าง 1 - 10
+    random_number = random.randint(1, 10)
+    print("== Guess Number between (1-10) ==")
+    guess_number = input("What is your guess number : ")
+    guess_number = int(guess_number)
+    print("number is ", random_number)
+    
+
+    if random_number == guess_number:
+        print("Congratulations")
+
+    elif random_number < guess_number:
+        print("Too high")
+
+    elif random_number > guess_number:
+        print("Too low")
+
+    #print(random_number)
     
 test_random()
